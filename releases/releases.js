c62be043c7ea6654fea17d4c620e5aeee1423120
Leônnyo <leowlopez@live.com>
2025-10-10 14:53:12 -0300

    test

--- releases/releases.js
+++
@@ -1,8 +0,0 @@
-fetch('latest.json')
-  .then(res => res.json())
-  .then(data => {
-    document.querySelector('#latestRelease h2').innerText = `Versão: ${data.version}`;
-    document.getElementById('notes').innerText = data.notes;
-    document.getElementById('downloadLink').href = data.url;
-  })
-  .catch(err => console.error('Erro ao carregar releases:', err));
